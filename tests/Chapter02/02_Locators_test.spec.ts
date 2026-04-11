b434032151d76e161e90d7ba643e44e14a1aca58
Wattanachai Boonchai <wattanachai.work@hotmail.com>
2026-04-12 02:47:10 +0700

    locator type

---
+++ tests/Chapter02/02_Locators_test.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+test('Locators in Playwright', async ({ page }) => {
+    //await page.goto('https://github.com/MxnlodyX');
+    // get By Role
+    // await page.getByRole('link',{name : 'Sign in'}).click();
+
+    // get By Label
+    // await page.getByLabel('Homepage',{exact: true}).first().click();
+
+    // get by Alt Attribute - For Images
+    // await page.getByAltText('MxnlodyX').click();
+
+    // get by TestID (F12 Tag : data-tab-item="XXXXXXXX")
+    // await page.getByTestId("repositories").first().click();
+
+    // get by text
+    // await page.getByText('Sign up').first().click();
+
+    // get by placeholder , CSS Selector , XPATH Selector
+    // await page.goto("https://www.youtube.com/@testerstalk");
+    // await page.getByPlaceholder('Search').first().click();
+    // await page.locator('.yt-searchbox-input').first().fill('Playwright Tutorial');
+    // await page.locator('//*[@id="center"]/yt-searchbox/div[1]/button').first().click();
+
+    // get by title (F12 Tag : title="XXXXXXXX")
+    // await page.goto("https://www.google.com/");
+    // await page.getByTitle('ค้นหา').first().fill("playwright by testers talk");
+
+
+}); 
