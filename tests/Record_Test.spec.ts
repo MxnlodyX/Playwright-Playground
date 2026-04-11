fb408a5c3d1f7cccd8f58faac79c642f87fcda19
Wattanachai Boonchai <wattanachai.work@hotmail.com>
2026-04-11 16:53:58 +0700

    manual recording test

---
+++ tests/Record_Test.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('GitHub Sign In Test : Invalid Credentials ', async ({ page }) => {
+  await page.goto('https://github.com/');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).fill('TestDummyInut');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('12345678');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+});
