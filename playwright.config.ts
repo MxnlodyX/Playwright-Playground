51f1dbbcca914a55899b8ee8ac6f021e7981dfff
Wattanachai Boonchai <wattanachai.work@hotmail.com>
2026-05-24 17:36:24 +0700

    add new dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    viewport: { width: 1920, height: 1080 },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     testIdAttribute : 'data-tab-item',
     screenshot : 'on',
